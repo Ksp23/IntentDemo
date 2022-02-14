070433dafb164d55c43ee3aa26404c0236310a53
PanielK <panielkee@gmail.com>
2022-02-14 13:55:32 +0800

    v1 of prac 3.3

---
+++ app/src/main/java/com/example/intentdemo/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.intentdemo
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val btnA: Button = findViewById(R.id.btnA)
+        btnA.setOnClickListener()
+        {
+            //variable name : Data Type , this is to redirect the button A to another page
+            val intentA: Intent = Intent(this, AboutActivity::class.java)
+
+            //key
+            intentA.putExtra("StudentName","John")
+
+            startActivity(intentA)
+        }
+
+        val btnB: Button = findViewById(R.id.btnB)
+        btnB.setOnClickListener(){
+            //action dial is to make a phone call
+            val geo = Uri.parse("geo:3.140853, 101.6869")
+            val intentB: Intent = Intent(Intent.ACTION_VIEW,geo)
+            startActivity(intentB)
+        }
+
+    }
+}
